ccdce8ead6e57c432819ea6ad5e45b90cd2eae16
dikshya-in-github <ghisingdeeku@email.com>
2024-03-29 12:07:55 +0545

    new shortcuts

--- js/script.js
+++ js/script.js
@@ -14,19 +14,9 @@ let topics = [
 ]
 
 for (let index = 0; index < topics.length; index++){
-    if (topics[index].status == true){               
+    if (topics[index].status){               
         console.log(`${topics[index].title} is completed`);
     } else{        
         console.log(`${topics[index].title} is pending`);        
     }
 } 
-
-/* let index= 0;
-if (topics[index].status == true){
-    console.log(`${topics[index].title} is completed`);
-    topics[index]++;
-}else{    
-    console.log(`${topics[index].title} is pending`);
-    topics[index]++;
-} 
- */

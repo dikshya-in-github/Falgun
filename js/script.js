afbc783218fd1fa8f92a24997759da9c8401337c
dikshya-in-github <ghisingdeeku@email.com>
2024-04-01 14:28:02 +0545

    feat: contitional statements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//loop and conditional statement
 let topics = [
     {
         title: "html",
@@ -20,3 +21,34 @@ for (let index = 0; index < topics.length; index++){
         console.log(`${topics[index].title} is pending`);        
     }
 } 
+
+
+//Logical operators
+let student = {
+    name: "Ram",
+    paid: false,
+    hasScholarship: true,
+    hasMetAttendace: true
+}
+// let {paid, hasScholarship, hasMetAttendace} = student;   //Object destructuring
+/* if ( (student.paid || student.hasScholarship) && (student.hasMetAttendace) ){
+    console.log("Ram can give exam");    
+}else{
+    console.log("Ram can't give exam");
+}  */
+
+
+//Ternary Operator
+// student.paid || student.hasScholarship ?  console.log("ram can give exam"): console.log("ram can't give exam");
+console.log(`ram ${(student.paid || student.hasScholarship) && (student.hasMetAttendace)? "can" : "cannot" } give exam`);
+
+
+//For each function
+    /* let number = [1,2,4,3]
+    // for (let i =0; i<number.length;i++){
+    //     console.log(number[i]);
+    // }
+
+    number.forEach((Element)=>{
+        console.log(Element*2);
+    }) */

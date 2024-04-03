504570ff5eb44d4c83502ddf8f2e8ea588e50e80
dikshya-in-github <ghisingdeeku@email.com>
2024-04-03 10:06:00 +0545

    task

--- js/index.js
+++ js/index.js
@@ -1,106 +1,41 @@
-console.log("this is js file");
-
-//Variable
-greeting = "namaste";
-console.log(greeting, "ram");
-console.log(greeting, "shyam");
-console.log(greeting, "sita");
-console.log(greeting, "gita");
-
-
-//Let vs Const
-let color;
-color = "red";
-
-const NAME = "Ram";
-
-console.log(color);
-console.log(NAME);
-
-//Undefined
-let subject;
-console.log(subject);
-
-// Array
-let brands = ["apple", "samsung", "lg", "soni"];
-
-console.log(brands);
-
-console.log("before", brands[3]);
-brands[3] = "sony";
-console.log("after", brands[3]);
-
-let courses = ["python", "QA", "Web", "graphics", "AI"];
-courses[4] = "Artificial Intelligence";
-console.log(courses);
-
-//Object
-let projector = {
-  brand: "viewsonic",
-  color: "white",
-  price: 50000,
-};
-
-console.log(projector);
-
-let company = {
-  title: "mindriser",
-  url: "https://worng.com",
-  estd: "2018",
-  isOpen: true,
-};
-console.log(company);
-
-//Bracket Notation
-/* company['url'] = "https://correct.com";
-    console.log(company); */
-
-//Dot Notation
-company.url = "www.correct.com";
-console.log(company);
-
-/* //Spread Operator
- let newCompany = {...company, url: 'www.correct.com'};
-    console.log(newCompany);
-
-//Object.assign Method
- let newCompany = Object.assign({}, company, {url : 'https://correct.com'})
-    console.log(newCompany); */
-
-let course = {
-  title: "web",
-  duration: 1,
-  startTime: 0,
-  endTime: 4,
-};
-
-course.startTime = 3;
-console.log(course);
-//new field
-let fieldToBEChange = "endTime";
-
-console.log(course["endTime"]);
-console.log(course[fieldToBEChange]);
-
-//Nested Object
-let user = {  
-    fullName: "Ram Tamang",
-    address: {
-      permanentAddress: {
-        district: "Bhaktapur",
-        city: "Suryabinayak",
-        ward: 7,
-      },
-      temporaryAddress: {
-        district: "Kathmandu",
-        city: "Putalisadak",
-        ward: 17,
-      },
+//Task
+let users = [
+    {
+        name: "Ram",
+        cartItems: [
+            { product: "watch", price: 1000 }
+        ],
     },
-    phoneNumber: {
-        ncellNums: ["00000", "0000"],
-        ntcNums: ["00000", "0000"],
-        lanLine: "0000",
-    }
+    {
+        name: "Hari",
+        cartItems: [
+            { product: "keyboard", price: 2000 },
+            { product: "mouse", price: 500 },
+        ],
+    },
+    {
+        name: "Shyam",
+        cartItems: [
+            { product: "mousePad", price: 500 },
+            { product: "mouse", price: 1000 },
+        ],
+    },
+] 
+
+const totalAmount = () => {    
+    for (let i = 0; i < users.length; i++){
+        let sum = 0       
+        let {name, cartItems} = users[i]
+        for (let index = 0; index < cartItems.length; index++) {
+            sum += cartItems[index].price                      
+        }
+        console.log(`${name} has ordered of Rs.${sum}`);       
+    }    
 }
-console.log(user);
+totalAmount()
+/* 
+    TODO: expected output
+    Ram has ordered of Rs.1000
+    Hari has ordered of Rs.2500
+    Shyam has ordered of Rs.1500
+*/

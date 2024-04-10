3b4222c446d9ece0d50a819fc91409dbdddee597
dikshya-in-github <ghisingdeeku@email.com>
2024-04-10 11:51:05 +0545

    feat: array methods

--- js/index.js
+++ js/index.js
@@ -1,41 +1,11 @@
-//Task
-let users = [
-    {
-        name: "Ram",
-        cartItems: [
-            { product: "watch", price: 1000 }
-        ],
-    },
-    {
-        name: "Hari",
-        cartItems: [
-            { product: "keyboard", price: 2000 },
-            { product: "mouse", price: 500 },
-        ],
-    },
-    {
-        name: "Shyam",
-        cartItems: [
-            { product: "mousePad", price: 500 },
-            { product: "mouse", price: 1000 },
-        ],
-    },
-] 
+ //Map method
+let nepaliNames = ["Aarav", "Aarya", "Aasha", "Anisha"];
+let newUsers = nepaliNames.map((name, idx) =>{
+    return {
+        name: name,
+        email: name.toLowerCase()+"@gmail.com",
+        password: name.toLowerCase()+idx
+    }
+})
 
-const totalAmount = () => {    
-    for (let i = 0; i < users.length; i++){
-        let sum = 0       
-        let {name, cartItems} = users[i]
-        for (let index = 0; index < cartItems.length; index++) {
-            sum += cartItems[index].price                      
-        }
-        console.log(`${name} has ordered of Rs.${sum}`);       
-    }    
-}
-totalAmount()
-/* 
-    TODO: expected output
-    Ram has ordered of Rs.1000
-    Hari has ordered of Rs.2500
-    Shyam has ordered of Rs.1500
-*/
+console.log(newUsers);
